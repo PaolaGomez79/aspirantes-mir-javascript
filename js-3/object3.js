8fe897e582d1c6f640b41eed7cb6331b77881af4
PaolaGomez79 <paomeli79@gmail.com>
2023-05-15 22:49:57 -0300

    Agrego código al ejercicio 7

--- js-3/object3.js
+++ js-3/object3.js
@@ -25,3 +25,11 @@ console.log(receta.ingredientes[0].nombre);
 // 7-
 const totalCantidad = receta.ingredientes.reduce((acumulador, ingrediente) => acumulador + ingrediente.cantidad, 0);
 console.log("La cantidad total de ingredientes es: " + totalCantidad);
+
+// 7- Sin utilizar el método reduce
+let cantidadTotal = 0;
+for (let i = 0; i < receta.ingredientes.length; i++) {
+  cantidadTotal += receta.ingredientes[i].cantidad;
+};
+
+console.log("La cantidad total de ingredientes es: " + cantidadTotal);
